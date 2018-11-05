0d64faa2b124841547a479012a43113efdcc29f4
KvNGCzA <akanmuoluwadamilare@yahoo.com>
2018-11-05 13:41:41 +0100

    feature(routes): create home and login route
    - change facebook success redirect route
    - change facebook failure redirect route
    - change minor test command
    - add home page route
    - add login page route
    
    [Finishes #161291006]

--- routes/api/users.js
+++ routes/api/users.js
@@ -34,16 +34,4 @@ router.get('/auth/facebook', facebookPassportRoutes.authenticate());
 // facebook callback route
 router.get('/auth/facebook/callback', facebookPassportRoutes.callback());
 
-// facebook success page
-router.get('/auth/facebook/success', (req, res) => res.status(200).json({
-  message: 'Successfully logged in with facebook!',
-  status: 200
-}));
-
-// facebook failure page
-router.get('/auth/facebook/failure', (req, res) => res.status(401).json({
-  message: 'Facebook login failed!',
-  status: 401
-}));
-
 export default router;
