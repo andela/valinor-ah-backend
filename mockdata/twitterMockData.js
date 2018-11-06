a748da98cdfe27badd87fbf2fb92ea5f33e21bed
Tersoo Atsen <tersoo.atsen@outlook.com>
2018-11-06 18:16:58 +0100

    feat(social-signup): add twitter social signup
    - add tests
    - add twitter routes
    - add twitter strategy
    - add twitter callback
    - set fullname column to accept nulls
    
    [Finishes #161648143]

---
+++ mockdata/twitterMockData.js
@@ -0,0 +1,14 @@
+export const profile = {
+  id: 12312309890,
+  emails: [{
+    value: 'tersoo@example.com'
+  }],
+  name: 'Ter',
+  photos: [{
+    // eslint-disable-next-line max-len
+    value: 'http://pbs.twimg.com/profile_images/979411998923542534/zqiYi9ih_normal.jpg'
+  }]
+};
+
+export const accessToken = '201934290-7d9fQUAPSFniOXmvcFRXqfHUTKSWeIg6JlY6Z4Vg';
+export const tokenSecret = 'PeKbqpDgt6kjX98ptSZYueGRIDILe4tMmAbch95YlNwhK';
