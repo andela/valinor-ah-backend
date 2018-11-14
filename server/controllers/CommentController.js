a1ba98fdbbdc4746865b40af5aaed937a21f76cf
sulenchy <sulaimanabiodun172@gmail.com>
2018-11-14 21:24:26 +0100

    feat(like-dislike-comment): add like dislike comment feature
    - add likeOrDislikeComment controller
    - add likeOrDislikeComment route
    - test likeOrDislikeComment endpoint
    - refactor validateArticleId middleware
    
    [Finishes #161291025]

--- server/controllers/CommentController.js
+++ server/controllers/CommentController.js
@@ -1,6 +1,10 @@
+import Sequelize from 'sequelize';
 import models from '../models';
 
-const { User, Comment } = models;
+const { User, Comment, CommentLike } = models;
+
+const { Op } = Sequelize;
+
 /**
  * @class CommentController
  * @description Contains Comment related Operations
@@ -53,5 +57,104 @@ class CommentController {
         }
       }));
   }
+
+  /**
+   * controller to like or dislike an commentId
+   * @param {object} req
+   * @param {object} res
+   * @param {object} next
+   * @returns {void}
+   */
+  static async likeOrDislikeComment(req, res, next) {
+    const { commentId, action } = req.params;
+    const userId = req.userData.id;
+
+    /**
+     * This function likes or dislikes an comment
+     * @param {boolean} status - true for like, false for dislike
+     * @returns {void}
+     */
+    const likeDisliker = async (status) => {
+      let commentLike;
+      let created;
+      try {
+        // find the comment like or create it
+        [commentLike, created] = await CommentLike.findOrCreate({
+          where: { [Op.and]: [{ userId }, { commentId }] },
+          defaults: { status, commentId, userId, }
+        });
+      } catch (err) {
+        return next(err);
+      }
+
+      // suffix for message
+      const suffix = status ? 'like' : 'dislike';
+      // get the article like id
+      const commentLikeId = commentLike.dataValues.id;
+
+      if (created) {
+        // if like/dislike was added
+        return res.status(201).json({
+          status: 'success',
+          type: suffix,
+          commentLikeStatus: true,
+          message: `comment successfully ${suffix}d`,
+        });
+      }
+
+      // if like/dislike was not added
+      if (status !== commentLike.dataValues.status) {
+        // opposite action was triggered, update the like
+        const [rowCount] = await CommentLike.update({
+          status: !commentLike.dataValues.status,
+        }, {
+          where: {
+            id: commentLikeId,
+          }
+        });
+        if (rowCount > 0) {
+          return res.status(200).json({
+            status: 'success',
+            type: suffix,
+            commentLikeStatus: true,
+            message: `you changed your mind, comment successfully ${suffix}d`,
+          });
+        }
+      }
+      // same action was triggered, undo the like or dislike
+      const undoRows = await CommentLike.destroy({
+        where: {
+          id: commentLikeId,
+        }
+      });
+      if (undoRows > 0) {
+        return res.status(200).json({
+          status: 'success',
+          type: suffix,
+          commentLikeStatus: false,
+          message: `comment ${suffix} reversed successfully`,
+        });
+      }
+    };
+
+    // error for unknown action in switch statement
+    const unknownActionError = new Error(
+      'unknown action, you may either like or dislike only'
+    );
+    unknownActionError.status = 422;
+
+    // switch statement for like or dislike
+    switch (action) {
+      case 'like':
+        likeDisliker(true);
+        break;
+      case 'dislike':
+        likeDisliker(false);
+        break;
+      default:
+        return next(unknownActionError);
+    }
+  }
 }
+
 export default CommentController;
